3aaa6a373a0a110769ebdccb546140d0e62781f5
Wenchuan Luo <wench@Serena>
2025-10-11 17:11:43 -0500

    roll back to initial status

--- app/domain/MenuFunctions.py
+++
@@ -1,7 +0,0 @@
-from typing import Callable
-
-class MenuFunctions:
-    def __init__(self, executor: Callable|None = None, printer: Callable|None = None, navigator: Callable|None = None):
-        self.executor = executor
-        self.printer = printer
-        self.navigator = navigator
--- app/main.py
+++
@@ -1,4 +0,0 @@
-from cli.menu_printer import print_menu
-from cli import constants
-
-print_menu(constants.LOGIN_MENU)
